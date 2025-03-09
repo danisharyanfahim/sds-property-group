bdf224b96b9397baf5b7208d4bf27f1ea769cf8f
Danish Fahim <danisharyanfahim@gmail.com>
2025-03-09 19:13:26 -0400

    finished contact page and stlying

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -1,16 +1,21 @@
+import ContactForm from "@/app/components/ui/contact-form";
 import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <main id="contact" className="page-section">
-      <section id="contact-us">
-        <h2>Contact Us</h2>
-        <h4>
-          Ready to explore your options? Reach out to us today and take the
-          first step toward unparalleled real estate success.
-        </h4>
-        <div className="contact-form">
-          <h4>Contact Form</h4>
+      <section id="contact-us" className="stacked">
+        <div className="info-section">
+          <FaPhoneAlt className="icon" />
+          <h2>Contact Us</h2>
+          <h4>
+            Ready to explore your options? Reach out to us today and take the
+            first step toward unparalleled real estate success.
+          </h4>
+        </div>
+        <div className="media-section">
+          <ContactForm />
         </div>
       </section>
     </main>
